20599adedce102b7825d383817d720f713647b09
toifatululum <toifatululum222@gmail.com>
2020-01-01 20:23:37 +0700

    add modules

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ connectDB();
 app.get("/", (req, res) => res.send("API running"));
 
 //Define routers
-app.use("api/users", require("./routes/api/users"));
-app.use("api/auth", require("./routes/api/auth"));
-app.use("api/profile", require("./routes/api/profile"));
-app.use("api/posts", require("./routes/api/posts"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
