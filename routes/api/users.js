bba3e6a8e930c12b848d5a3011033f8186ab475e
toifatululum <toifatululum222@gmail.com>
2020-01-02 20:27:27 +0700

    add check

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-//@route  Get api/users
-//@desc   Test route
+//@route  Post api/users
+//@desc   Register User
 //@access Public
-router.get("/", (req, res) => res.send("User route"));
-
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email")
+      .not()
+      .isEmpty(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ mi: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //connect Database
 connectDB();
 
+// init midleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running"));
 
 //Define routers
