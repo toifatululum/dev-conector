3887d8dc99a63fec3071bb4caed9192bc3b3fc51
toifatululum <toifatululum222@gmail.com>
2020-01-12 08:58:41 +0700

    get post by ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -48,4 +48,38 @@ router.post(
   }
 );
 
+//@route  Getapi/posts
+//@desc   get a post
+//@access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route  Getapi/posts/:id
+//@desc   get post by id
+//@access Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const posts = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "post not found" });
+    }
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
