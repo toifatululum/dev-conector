f4a69736c403111da2ec2fa433e7f9d229e6cdc5
toifatululum <toifatululum222@gmail.com>
2020-01-20 22:13:40 +0700

    add comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -52,7 +52,6 @@ router.post(
 //@desc   get a post
 //@access Private
 router.get("/", auth, async (req, res) => {
-  this.ulum("data");
   try {
     const posts = await Post.find().sort({ date: -1 });
     res.json(posts);
@@ -207,9 +206,40 @@ router.post(
     }
   }
 );
+//@route  Post api/posts/comment/:id
+//@desc   Comment on a post
+//@access Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
-const ulum = data => {
-  console.log(data);
-};
+    //pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    //make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "comment does not exist" });
+    }
+    //Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(404).json({ msg: "comment does not exist" });
+    }
+
+    //get remove index
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
