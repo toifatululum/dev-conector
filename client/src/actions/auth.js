d9b3e3c69586822b6ef7a50a21b5c55444a3a5af
toifatululum <toifatululum222@gmail.com>
2020-02-19 09:13:06 +0700

    starting dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCES,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from "./types";
 
 import setAuthToken from "../utils/setAuthToken";
@@ -85,5 +86,6 @@ export const login = (email, password) => async dispatch => {
 
 //Logout/ clear profile
 export const logout = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,4 +8,5 @@ export const LOGIN_SUCCES = "LOGIN_SUCCES";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
 export const GET_PROFILE = "GET_PROFILE";
+export const CLEAR_PROFILE = "CLEAR_PROFILE";
 export const PROFILE_ERROR = "PROFILE_ERROR";
